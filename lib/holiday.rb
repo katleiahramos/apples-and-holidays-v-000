f993def92f90f8fae96d8a878b82e4a018e4355b
Katleiah Ramos <katleiahramos@gmail.com>
2018-03-13 17:36:16 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ def all_supplies_in_holidays(holiday_hash)
       holiday_s = holiday.to_s.split(' ')
       if holiday_s.length == 1
         binding.pry
-        puts "#{holiday_s[0].capitalize!}"
+        puts "#{holiday_s[0].capitalize!}:"
       if holiday_s > 1
         holiday_s.each do |word|
         word.capitalize!
